b372b58823bbe6cc2744a021944644529b62d70b
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-09-17 15:56:17 +0000

    event listener

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,22 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector("#register");
 // Handle form submit event
-registerForm.onsubmit = function(event) {
-    event.preventDefault();
-    console.log(event);
+registerForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Collect form data
+  const formData = new FormData(registerForm);
+  // Send data to backend
+  // display success message to user
+  const messageH1 = document.querySelector('#message');
+  messageH1.textContent = 'Account registered successfully'
+});
+
+// declare variable age
+// let age = 71
+// define square age function
+function squareAge (age) {
+  let new_age = age * age
+  console.log(new_age)
 }
+
+squareAge(56)
